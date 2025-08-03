4bc628b96ee9e09e86adab4e13830b0f3d8bd20e
Reema_abde <Reemabdella702@gmail.com>
2025-08-03 20:05:15 +0300

    feat:Modified Post.js

--- back-end/models/Post.js
+++ back-end/models/Post.js
@@ -1,12 +1,18 @@
 import mongoose from 'mongoose';
 
+const commentSchema = new mongoose.Schema({
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  text: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now }
+});
+
 const postSchema = new mongoose.Schema({
   title: { type: String, required: true },
   content: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-}, {
-  timestamps: true,
-});
+  author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
+  comments: [commentSchema],
+}, { timestamps: true });
 
 const Post = mongoose.model('Post', postSchema);
 
