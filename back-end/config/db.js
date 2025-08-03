7f71c51561d4cac429a47b3af64b6d368220756f
Reema_abde <Reemabdella702@gmail.com>
2025-08-03 19:46:43 +0300

    Updated backend branch

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -3,15 +3,14 @@ import mongoose from "mongoose";
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     });
-
-    console.log(🌐 MongoDB Connected: ${conn.connection.host});
-} catch (error) {
-    console.error(❌ Error: ${error.message});
+    console.log(`🌐 MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`❌ Error: ${error.message}`);
     process.exit(1); // Exit process with failure
-}
+  }
 };
 
 export default connectDB;
