5a4d766a79337634265650ec69982d5f438efed4
Reema_abde <Reemabdella702@gmail.com>
2025-08-03 11:30:20 +0300

    Implement basic user model for authentication

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -9,4 +9,21 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: true,
         unique: true,
-      },
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    role: {
+        type: String,
+        enum: ["user", "admin"],
+        default: "user",
+    },
+    followers: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    }]
+    }, { timestamps: true });
+    
+export default mongoose.model("User", userSchema);
+
