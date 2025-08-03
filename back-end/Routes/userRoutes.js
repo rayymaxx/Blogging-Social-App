463bf2ba23ab97c88ddc161a93afcdb6e64b9b72
Reema_abde <Reemabdella702@gmail.com>
2025-08-03 19:58:38 +0300

    Added user-Routes

---
+++ back-end/Routes/userRoutes.js
@@ -0,0 +1,8 @@
+import express from "express";
+const router = express.Router();
+import { registerUser, authUser } from "../controllers/userController.js";
+
+router.post("/register", registerUser);
+router.post("/login", authUser);
+
+export default router;
